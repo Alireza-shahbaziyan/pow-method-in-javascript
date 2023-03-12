7bd0d6a156092fc4c1540840966221e325e305a6
Alireza shahbaziyan <87496598+Alireza-shahbaziyan@users.noreply.github.com>
2023-03-12 12:41:19 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function pow(x, n) {
-  return (n == 1) ? x : (x * pow(x, n - 1));
-}
+const pow = (x, n) => (n == 1) ? x : (x * pow(x, n - 1));
+// function pow(x, n) {
+//   return (n == 1) ? x : (x * pow(x, n - 1));
+// }
+
+// function pow(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+// alert( pow(2, 3) ); // 8
+
+
